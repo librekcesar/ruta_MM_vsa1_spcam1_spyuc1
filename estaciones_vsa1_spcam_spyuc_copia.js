1b5eb4d5be8427781d016c2583417488ae3cf1e2
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 15:48:06 -0600

    Add files via upload

---
+++ estaciones_vsa1_spcam_spyuc_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_vsa1_spcam_spyuc",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "C. Miguel Hidalgo y Costilla 245, Tamulte de las Barrancas, 86150 Villahermosa, Tab.", "description": "VSA1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -92.956401, 17.9701173, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Av. Antigua Kalá 6, Zona Sin Asignación de Nombre, 24087 San Francisco de Campeche, Camp.", "description": "SPCAM1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -90.4913601, 19.8526316, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Kanasín, Yucatán", "description": "SPYUC1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -89.5639005, 20.944148, 0.0 ] } }
+]
+}
---
+++ paradas_vsa1_spcam_spyuc_copia.js
@@ -0,0 +1,11 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_vsa1_spcam_spyuc",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Av. 10 de Julio, Puntilla, 24139 Cdad. del Carmen, Camp.", "layer": "PEMEX_VSA1_CAM1 — PEMEX_VSA1_CZM+CAM1", "direccion": "Av. 10 de Julio, Puntilla, 24139 Cdad. del Carmen, Camp." }, "geometry": { "type": "Point", "coordinates": [ -91.8252051, 18.631675, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Ejido No. 169, Centro, 86750 Frontera, Tab.", "layer": "PEMEX_VSA1_CAM1 — PEMEX_VSA1_CZM+CAM1", "direccion": "Ejido No. 169, Centro, 86750 Frontera, Tab." }, "geometry": { "type": "Point", "coordinates": [ -92.635845, 18.529802, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "bp", "description": "Champoton-Escarcega Carretera Champoton Isla Aguada, Las Brisas, 24400 Champotón, Camp.", "layer": "PEMEX_VSA1_CAM1 — PEMEX_VSA1_CZM+CAM1", "direccion": "Champoton-Escarcega Carretera Champoton Isla Aguada, Las Brisas, 24400 Champotón, Camp." }, "geometry": { "type": "Point", "coordinates": [ -90.7353256, 19.3365819, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": null, "layer": "PEMEX_VSA1_CAM1 — PEMEX_VSA1_CZM+CAM1", "direccion": null }, "geometry": { "type": "Point", "coordinates": [ -90.223688, 20.030846, 0.0 ] } }
+]
+}
